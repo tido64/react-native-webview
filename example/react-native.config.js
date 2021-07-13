278695343b8c6ff190a9fee38839062481918417
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-07-13 15:00:30 +0200

    migrated to react-native-test-app

---
+++ example/react-native.config.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+const windowsProjectFile = path.join(
+  "node_modules",
+  ".generated",
+  "windows",
+  "ReactTestApp",
+  "ReactTestApp.vcxproj"
+);
+
+module.exports = {
+  project: {
+    android: {
+      sourceDir: "android",
+      manifestPath: path.relative(
+        path.join(__dirname, "android"),
+        path.join(
+          path.dirname(require.resolve("react-native-test-app/package.json")),
+          "android",
+          "app",
+          "src",
+          "main",
+          "AndroidManifest.xml"
+        )
+      ),
+    },
+    ios: {
+      project: "ios/ReactTestApp-Dummy.xcodeproj",
+    },
+    windows: fs.existsSync(windowsProjectFile) && {
+      sourceDir: "windows",
+      solutionFile: "WebviewExample.sln",
+      project: {
+        projectFile: path.relative(
+          path.join(__dirname, "windows"),
+          windowsProjectFile
+        ),
+      },
+    },
+  },
+};
